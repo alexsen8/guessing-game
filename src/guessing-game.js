2c13774570715108915c35f01cdf1d5ca0ec8b3c
alexsen8 <104522882+alexsen8@users.noreply.github.com>
2022-08-16 19:00:30 +0300

    feat: add decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.minimum + this.maximum) / 2);
+        return this.result
     }
 
     lower() {
-
+        this.maximum = this.result
     }
 
     greater() {
-
+        this.minimum = this.result
     }
 }
 
